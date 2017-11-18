ce9ef27ff7c319a4049175b002e2688d1be99c34
huseyinsarikaya <gotrimes@gmail.com>
2017-11-18 14:23:25 +0300

    hesaplama minimum hüseyin sarıkaya

---
+++ MatematikUygulamasi/src/test/java/minimumTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+public class minimumTest {
+
+	
+	public void testMinHesapla30ve50() {
+		int sonuc= Math.min(30,50);
+		assertEquals(30, sonuc);
+	}
+	
+}
