93f626d3beb0a640afbebe7cfe95d79484421455
M.ARİCİ <mertcan.aricii@gmail.com>
2017-11-18 14:20:26 +0300

    Carpma

---
+++ MatematikUygulamasi/src/main/java/Carpma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Carpma {
+	/**
+	 * carpma islemi yapýlacak.
+	 * @param say1 ve say2 seklinde iki sayi istenecek ve girilen iki sayýyý carpýp sonucu bulacak.
+	 */
+	
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int say1,say2;
+        System.out.println("1. Sayýyý Giriniz: ");
+        say1 = input.nextInt();
+        System.out.println("2. Sayýyý Giriniz: ");
+        say2 = input.nextInt();
+        int carp=say1*say2;
+        System.out.println(say1 + "*" + say2 + "=" + carp);
+        
+        
+    }
+}
---
+++ MatematikUygulamasi/src/test/java/CarpmaTest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CarpmaTest {
+
+	
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int say1,say2;
+        System.out.println("1. Sayýyý Giriniz: ");
+        say1 = input.nextInt();
+        System.out.println("2. Sayýyý Giriniz: ");
+        say2 = input.nextInt();
+        int sonuc=say1*say2;
+        System.out.println(say1 + "*" + say2 + "=" + sonuc);
+        
+        
+    }
+}
