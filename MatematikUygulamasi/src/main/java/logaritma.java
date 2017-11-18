94cfd7aa5704be20d20ed3c73ac20dd3620a8328
ebubekirakman <ebubekirtr@hotmail.com>
2017-11-18 14:23:21 +0300

    Logaritma hesaplana test yazılmadı

---
+++ MatematikUygulamasi/src/main/java/logaritma.java
@@ -0,0 +1,17 @@
+
+public class logaritma {
+	
+		
+		
+	public static void main(String[] args) {
+			
+
+		 double x = 60984.1;
+	      double y = -497.99;
+		
+		
+	      System.out.println("Math.log(" + x + ")=" + Math.log(x));
+	      System.out.println("Math.log(" + y + ")=" + Math.log(y));
+		
+	}
+}
---
+++ MatematikUygulamasi/src/test/java/logaritmaTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class logaritmaTest {
+
+	
+		
+	}
+		
+		
+	
+	
+	
+	
+	
+	
+	
