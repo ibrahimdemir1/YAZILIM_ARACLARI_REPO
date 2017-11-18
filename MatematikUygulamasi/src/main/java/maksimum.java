f1e67be0687ad71fccee046d9b4d775e9c561cc8
yasarfaruk <bprlab@10.201.191.213>
2017-11-18 13:52:04 +0300

    hesaplama maxsimum yasarfarukyılmaz

---
+++ MatematikUygulamasi/src/main/java/maksimum.java
@@ -0,0 +1,13 @@
+public class maksimum 
+{
+ 
+    public static void main(String[] args) 
+    {
+       
+    	int sayi1 = 99;
+    int sayi2 = 88;
+        String Maksimum = "Maksimum: " + Integer.toString(Math.max(sayi1, sayi2));
+        System.out.println("Maksimum: " +  Maksimum); 
+       
+    }
+}
---
+++ MatematikUygulamasi/src/test/java/maksimumtest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class maksimumtest {
+	
+	 public void testMaxhesapla99ve88() {
+		 int sonuc = Math.max(99, 88);
+		 assertEquals(99, sonuc);
+	}
+	
+}
