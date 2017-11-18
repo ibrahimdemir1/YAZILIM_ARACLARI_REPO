b0c3a15663cb18994007c350f7d79b8761dbb908
Batuhan Kutluay <batuhankutluay@gmail.com>
2017-11-18 14:40:07 +0300

    Çıkarma

---
+++ MatematikUygulamasi/src/main/java/Cikarma.java
@@ -0,0 +1,12 @@
+
+public class Cikarma {
+
+	
+	public static void main(String[] args) {
+		System.out.println(Cikarma.hesapla(25, 10));
+	}
+	
+	public static int hesapla(int s1, int s2) {
+		return s1 - s2;
+	}
+}
---
+++ MatematikUygulamasi/src/test/java/CikarmaTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CikarmaTest {
+
+	
+	
+	@Test
+	public void testHesapla() {
+		assertEquals(15,Cikarma.hesapla(25, 10));
+	}
+	
+
+}
