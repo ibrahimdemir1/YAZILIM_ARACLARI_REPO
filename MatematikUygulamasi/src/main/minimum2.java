b937d6f39312eb5be8960fab0e2476e007980c9c
huseyinsarikaya <gotrimes@gmail.com>
2017-11-25 12:54:01 +0300

    huseyinsarikaya

---
+++ MatematikUygulamasi/src/main/minimum2.java
@@ -0,0 +1,13 @@
+public class minimum2 {
+	public static void main(String[] args) 
+	{
+		
+		int sonuc = Math.min(30, 50);
+		System.out.println(sonuc);
+	}
+	public static int Hesapla(int sayi1, int sayi2) {
+		
+		return Math.min(sayi1, sayi2);
+		
+	}
+}
